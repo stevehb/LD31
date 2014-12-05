e4a5240ec814386bd2d79e21dc92430b98b00700
Stephen Blackwell <stevehb+ticonderoga@gmail.com>
2014-12-05 14:46:46 -0500

    initial commit -- blank libGDX project

---
+++ desktop/src/com/sblackwell/ld31/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sblackwell.ld31.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sblackwell.ld31.LD31Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD31Game(), config);
+	}
+}
---
+++ html/src/com/sblackwell/ld31/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.sblackwell.ld31.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.sblackwell.ld31.LD31Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LD31Game();
+        }
+}
